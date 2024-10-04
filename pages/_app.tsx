3329ec157e76d55697b41b76364d174698ad1d01
Brendan Le <brendan.mi.le@outlook.com>
2024-10-04 16:46:43 -0600

    initial setup

---
+++ pages/_app.tsx
@@ -0,0 +1,48 @@
+// Import libraries
+import { AppProps } from "next/app";
+import Head from "next/head";
+
+// Import styles
+import "@styles/reset.scss";
+import "@styles/global.scss";
+import { EB_Garamond, Cormorant_Garamond } from "next/font/google"; // Import Fonts (Optimized By NextJS)
+
+// Configure fonts
+const eb_garamond = EB_Garamond({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-eb-garamond",
+  weight: ["400", "500"],
+});
+
+const cormorant_garamond = Cormorant_Garamond({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-cormorant-garamond",
+  weight: ["400", "500", "600"],
+});
+
+/**
+ * Used to initialize each page.
+ *
+ * Here, we import fonts, context providers, and etc.
+ * that need to be done once for the entire application.
+ *
+ * @component
+ */
+export default function CustomApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+      </Head>
+
+      <div className={`${eb_garamond.variable} ${cormorant_garamond.variable}`}>
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
+}
